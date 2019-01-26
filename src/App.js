386ecf642e3e14e91facf28dca37a8342eac598f
Izabela <izabela.nowosada@gmail.com>
2019-01-26 08:10:48 +0100

    add select option succesfully

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+
+
 
 class App extends Component {
+  
   render() {
+    console.log(this.props, 'my props - state in app')
+    console.log(this.props.computers)
+    const modelsList = Object.keys(this.props.computers)
+    console.log(modelsList);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      
+      <div className="App">      
+      <form >
+        <label>Choose a model:</label>
+            <select id="computer-select">
+              <label>
+                <option value="">--Pick a model--</option>
+              </label>
+                
+                {console.log(modelsList)}
+
+                { modelsList.map((model, i )=>  
+                  (
+                    <option value={model} key={'comp' + i}> {model}</option>
+                   ))}
+            </select>
+        <button>Add</button>
+        </form>
+
       </div>
     );
   }
 }
 
-export default App;
+
+const mapStateToProps = (state) => {
+  return {
+    computers: state
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ src/reducers/reducer.js
@@ -0,0 +1,34 @@
+
+const data = {
+  "Ivel Z3": {
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  "Bally Astrocade": {
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  "Sord M200 Smart Home Computer": {
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  "Commodore 64": {
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+}
+
+const InitialState = data;
+
+const reducer = (state = InitialState, action = {}) => {
+  switch (action.type) {
+  default:
+    return state
+  }
+}
+
+export default reducer
